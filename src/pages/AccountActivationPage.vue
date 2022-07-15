d73478244563134a3cd4684b7c6c5dcfeb15904a
bryan <bwbortner@gmail.com>
2022-07-14 20:09:53 -0600

    Add api call for account activation page

--- src/pages/AccountActivationPage.vue
+++ src/pages/AccountActivationPage.vue
@@ -1,5 +1,38 @@
 <template>
   <div data-testid="activation-page">
-    <h1>Account Activation</h1>
+    <div v-if="success" class="alert alert-success mt-3">
+      Account is activated
+    </div>
+    <div v-if="fail" class="alert alert-danger mt-3">
+      Activation failure
+    </div>
+    <span v-if="apiProgress" class="spinner-border" role="status"></span>
   </div>
 </template>
+
+<script>
+import { activate } from '../api/apiCalls';
+
+export default {
+  data() {
+    return {
+      success: false,
+      fail: false,
+      apiProgress: false,
+    };
+  },
+
+  async mounted() {
+    this.apiProgress = true
+
+    try {
+      await activate(this.$route.params.token)
+      this.success = true
+    } catch (error) {
+      this.fail = true
+    }
+    
+    this.apiProgress = false
+  },
+}
+</script>
