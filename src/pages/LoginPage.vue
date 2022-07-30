1578d14da467073a91a27cb31ab026b435201034
bryan <bwbortner@gmail.com>
2022-07-30 17:10:27 -0600

    Add elements to login page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,5 +1,45 @@
 <template>
-  <div data-testid="login-page">
-    <h1>Login Page</h1>
+  <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2" data-testid="login-page">
+    <form class="card mt-5">
+      <div class="card-header">
+        <h1 class="text-center">Login</h1>
+      </div>
+
+      <div class="card-body">
+        <GeneralInput
+          id="e-mail"
+          label="E-mail"
+          v-model="email"
+        />
+
+        <GeneralInput
+          id="password"
+          label="Password"
+          v-model="password"
+          type="password"
+        />
+
+        <div class="text-center">
+          <button class="btn btn-primary" disabled>Login</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
+
+<script>
+import GeneralInput from '../components/GeneralInput'
+
+export default {
+  components: {
+    GeneralInput
+  },
+
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
